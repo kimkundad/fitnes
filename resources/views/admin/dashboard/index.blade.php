dfbfff69da8cbadb3b6084c9ef0c5b47b12b4580
kimkundad <kim.kundad@gmail.com>
2019-01-03 16:01:42 +0700

    golf

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -141,52 +141,7 @@
 
                               </table>
 
-                              <br />
-                              <h5 class="card-title">ตาราง GT FITNESS</h5>
-                              <br />
 
-                              <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
-                                  <li class="nav-item">
-                                      <a role="tab" class="nav-link active" id="tab-0" data-toggle="tab" href="#tab-content-0">
-                                          <span>All</span>
-                                      </a>
-                                  </li>
-                                  <li class="nav-item">
-                                      <a role="tab" class="nav-link" id="tab-1" data-toggle="tab" href="#tab-content-1">
-                                          <span>Yoka</span>
-                                      </a>
-                                  </li>
-                                  <li class="nav-item">
-                                      <a role="tab" class="nav-link" id="tab-2" data-toggle="tab" href="#tab-content-2">
-                                          <span>Kick Boxing</span>
-                                      </a>
-                                  </li>
-                              </ul>
-
-
-                              <div class="tab-content">
-                                  <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
-                                      <div class="main-card mb-3 card">
-                                          <div class="card-body">
-                                              <div id='calendar'></div>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="tab-pane tabs-animation fade" id="tab-content-1" role="tabpanel">
-                                      <div class="main-card mb-3 card">
-                                          <div class="card-body">
-                                              <div id='calendar-list'></div>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="tab-pane tabs-animation fade" id="tab-content-2" role="tabpanel">
-                                      <div class="main-card mb-3 card">
-                                          <div class="card-body">
-                                              <div id="calendar-bg-events"></div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
 
 
 
@@ -195,6 +150,66 @@
                               </div>
 
                               <br />
+
+                              <div class="row">
+
+                                <div class="col-lg-12">
+
+
+
+
+
+
+
+                                    <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
+                                        <li class="nav-item">
+                                            <a role="tab" class="nav-link active" id="tab-0" data-toggle="tab" href="#tab-content-0">
+                                                <span>All</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a role="tab" class="nav-link" id="tab-1" data-toggle="tab" href="#tab-content-1">
+                                                <span>Yoka</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a role="tab" class="nav-link" id="tab-2" data-toggle="tab" href="#tab-content-2">
+                                                <span>Kick Boxing</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+
+
+                                    <div class="tab-content">
+                                        <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
+                                            <div class="main-card mb-3 card">
+                                                <div class="card-body">
+                                                    <div id='calendar'></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="tab-pane tabs-animation fade" id="tab-content-1" role="tabpanel">
+                                            <div class="main-card mb-3 card">
+                                                <div class="card-body">
+                                                    <div id='calendar-list'></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="tab-pane tabs-animation fade" id="tab-content-2" role="tabpanel">
+                                            <div class="main-card mb-3 card">
+                                                <div class="card-body">
+                                                    <div id="calendar-bg-events"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+                                </div>
+                                </div>
+
+
 
                               <div class="row">
 
