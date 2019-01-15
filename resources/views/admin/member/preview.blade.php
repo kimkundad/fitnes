32b10fc3b2b90001c481001b14c64cfc181f4c5f
kimkundad <kim.kundad@gmail.com>
2019-01-16 02:26:49 +0700

    golf

--- resources/views/admin/member/preview.blade.php
+++ resources/views/admin/member/preview.blade.php
@@ -97,40 +97,59 @@
 
                               </div>
 
-
+<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
 <style>
 .btn-danger {
+  font-family: 'Prompt', sans-serif;
     color: #fff;
     background-color: #e2141c;
     border-color: #e2141c;
 }
 .circle-icon {
+  font-family: 'Prompt', sans-serif;
     background: #ffc0c0;
     padding:30px;
     border-radius: 50%;
 }
+.dropdown-menu-header .menu-header-image {
+  font-family: 'Prompt', sans-serif;
+    position: relative;
+    left: 0;
+    top: 0;
+    height: 480px;
+    width: 100%;
+    z-index: 0;
+    opacity: 1;
+    filter: grayscale(0%);
+    background-size: cover;
+}
+
+.list-group-item {
+  font-family: 'Prompt', sans-serif;
+    position: relative;
+    display: block;
+    padding: .75rem 1.25rem;
+    margin-bottom: -1px;
+    background-color: #fff;
+    border: 1px solid rgba(255, 255, 255, 0.125);
+}
 </style>
 
 
-                              <div class="col-lg-6 offset-md-3" id="canvas">
+                              <div class="col-lg-6" >
 
-                                <div class="mb-3 profile-responsive card">
+                                <div class="mb-3 profile-responsive card" style="height:1366px; width:768px; font-family: 'Prompt', sans-serif!important; " id="canvas">
                                     <div class="dropdown-menu-header">
-                                        <div class="dropdown-menu-header-inner bg-dark" style="    background-color: #f30f1a !important;">
-                                            <div class="menu-header-image opacity-4" style="background-image: url('assets/images/dropdown-header/city1.jpg');"></div>
+                                        <div class="dropdown-menu-header-inner" style="   padding:0px">
+                                            <div class="menu-header-image" >
+                                            <img src="{{url('assets/images/123456.png')}}">
+                                          </div>
                                             <div class="menu-header-content" style="width:100%">
-                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine" style="width:100%">
-                                                <img src="{{url('assets/images/logo-inverse.png')}}" alt="Avatar 5">
 
-                                                        <br />
-                                                </div>
-                                                <div style="width:100%">
-                                                    <h5 class="menu-header-title">{{$objs->no_mem}}</h5>
 
-                                                </div>
-                                                <br />
-                                                <div class=" rounded" style="margin: 0 auto;">
-                                                    <img  class="rounded-circle" src="{{url('assets/images/avatar/'.$objs->image_mem)}}" style="width:200px; margin-bottom:-110px; border-radius: 50%;">
+
+                                                <div class=" rounded" style="margin: 0 auto; z-index:99;">
+                                                    <img  class="rounded-circle" src="{{url('assets/images/avatar/'.$objs->image_mem)}}" style="position: absolute; left: 170px; top: -230px; width:420px; height:420px; margin-bottom:-110px; border-radius: 50%;">
 
                                                 </div>
 
@@ -140,27 +159,54 @@
                                     </div>
                                     <ul class="list-group list-group-flush">
                                         <li class="p-0 list-group-item">
-                                            <div class="widget-content">
+                                            <div class="widget-content" style="height: 734px;">
                                                 <div class="text-center">
                                                   <div class="menu-header-btn-pane">
                                                     <br /><br /><br /><br />
 
-                                                    <table class="col-lg-12 offset-md-3" style = 'text-align:justify; width:100%'>
+                                                    <table class="col-lg-12 offset-md-4" style = 'margin-top: 120px; text-align:justify; width:100%'>
                                                       <tr>
                                                         <td>
-                                                            <h5 style="font-size: 1.20rem; color:#f30f1a" >ยินดีต้อนรับคุณ {{$objs->first_name_mem}} {{$objs->last_name_mem}}</h5>
+                                                            <h5 style="font-size: 36px;; color:#f30f1a" >ยินดีต้อนรับคุณ </h5>
 
                                                         </td>
                                                       </tr>
                                                     </table>
-                                                      <div  class="btn-wide btn-shadow  btn btn-danger">@if($objs->type_mem == 1)
+                                                    @if(strlen($objs->first_name_mem.''.$objs->last_name_mem) < 9)
+                                                    <table class="col-lg-12 offset-md-3" style = ' text-align:justify; width:100%'>
+                                                      <tr>
+                                                        <td>
+                                                            <h5 style="font-size: 36px;; color:#f30f1a" > {{$objs->first_name_mem}} {{$objs->last_name_mem}}</h5>
+                                                        </td>
+                                                      </tr>
+                                                    </table>
+                                                    @else
+                                                    <table class="col-lg-12 offset-md-4" style = ' text-align:justify; width:100%'>
+                                                      <tr>
+                                                        <td>
+                                                            <h5 style="font-size: 36px;; color:#f30f1a" > {{$objs->first_name_mem}} {{$objs->last_name_mem}}</h5>
+                                                        </td>
+                                                      </tr>
+                                                    </table>
+                                                    @endif
+                                                    <br />
+
+                                                    <table class="col-lg-12" style = 'margin-left: 300px; text-align:justify; width:100%'>
+                                                      <tr>
+                                                        <td>
+                                                      <div  class="btn-wide btn-shadow  btn btn-danger" style="font-size: 30px; text-align:justify;">@if($objs->type_mem == 1)
                                                       รายวัน
                                                       @elseif($objs->type_mem == 2)
                                                       รายเดือน
                                                       @else
                                                       รายปี
-                                                      @endif</div><br />
-                                                      <img style="margin:0 auto; width:200px;" class="img-responsive" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(300)->generate($objs->no_mem)) !!} "><br />
+                                                      @endif</div>
+                                                    </td>
+                                                  </tr>
+                                                </table>
+                                                      <br />
+
+                                                      <img style="margin:0 auto; width:320px;" class="img-responsive" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(300)->generate($objs->no_mem)) !!} "><br />
                                                   </div>
                                                 </div>
                                             </div>
@@ -169,12 +215,13 @@
                                             <div class="grid-menu grid-menu-2col" style="background-color: #343a40 !important;">
                                                 <div class="no-gutters row">
                                                     <div class="col-sm-12">
-                                                        <div class="p-1">
-                                                          <table class="col-lg-12 offset-md-3" style = 'text-align:justify; width:100%'>
+                                                        <div class="p-1" style="padding: 30px !important; height: 150px;">
+                                                          <table class="col-lg-12 offset-md-1" style = 'text-align:justify; width:100%'>
                                                             <tr>
                                                               <td>
-                                                                  <h5 style="font-size: 1.20rem; color:#fff" >หมดอายุสมาชิก <?php echo DateThai($objs->end_at); ?></h5>
-                                                                  <h5 class="col-lg-12 offset-md-1" style="font-size: 13px; color:#fff" >ออกบัตร <?php echo DateThai($objs->start_at); ?></h5>
+                                                                  <h5 style="font-size: 2.8rem; color:#fff; padding-left:30px;" >หมดอายุสมาชิก <?php echo DateThai($objs->end_at); ?></h5>
+
+                                                                  <h5 class="col-lg-12 offset-md-3" style="font-size: 24px; color:#fff">ออกบัตร <?php echo DateThai($objs->start_at); ?></h5>
                                                               </td>
                                                             </tr>
                                                           </table>
@@ -188,7 +235,7 @@
                                 </div>
 
                                 </div>
-                                <div class="col-lg-6 offset-md-3">
+                                <div class="col-lg-6 offset-md-2">
                                 <a class="colormycanvas mb-2 mr-2 btn-pill btn btn-secondary" href="{{url('admin/member')}}">เสร็จสิ้น
                                 </a>
                                 <button class="colormycanvas mb-2 mr-2 btn-pill btn btn-secondary" id="colormycanvas">DOWNLOAD
