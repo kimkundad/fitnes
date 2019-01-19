2f6e1d0921fa5748742b734e84112773e02183d2
kimkundad <kim.kundad@gmail.com>
2019-01-19 18:12:32 +0700

    sdsd

--- resources/views/admin/member/check_in_member.blade.php
+++ resources/views/admin/member/check_in_member.blade.php
@@ -3,35 +3,6 @@
 @section('admin.content')
 
 
-<div class="app-page-title">
-                        <div class="page-title-wrapper">
-                            <div class="page-title-heading">
-                                <div class="page-title-icon">
-                                    <i class="pe-7s-users icon-gradient bg-tempting-azure">
-                                    </i>
-                                </div>
-                                <div>เช็คอินสมาชิก GT Fitnes
-                                    <div class="page-title-subheading">เช็คอินสมาชิก ของ GT Fitnes ทั้งหมด
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="page-title-actions">
-                              <a href="{{url('admin/member/create')}}"  class="btn-shadow btn btn-info">
-                                  <span class="btn-icon-wrapper pr-2 opacity-7">
-                                      <i class="icon ion-android-add"></i>
-                                  </span>
-                                  สมัครสมาชิก
-                              </a>
-
-                            </div>
-
-
-
-                          </div>
-                    </div>
-
 <div class="tabs-animation">
                         <div class="row">
                             <div class="col-lg-6 offset-md-3">
@@ -41,10 +12,10 @@
                                 <div class="card-body"><h5 class="card-title">ค้นหาสมาชิก</h5>
 
                                   <br />
-                                  <div class="search-wrapper active" style="margin-top:5px;">
-                                    <form class="" method="POST" action="{{url('admin/search_mem_checkin')}}" enctype="multipart/form-data" novalidate="novalidate">
+                                  <div class="search-wrapper active" style="margin-top:5px; width: 100%;">
+                                    <form class="" method="POST"  action="{{url('admin/search_mem_checkin')}}" enctype="multipart/form-data" novalidate="novalidate">
                                     {{ csrf_field() }}
-                                      <div class="input-holder" style="width: 450px;">
+                                      <div class="input-holder" style="width: 100%;">
                                           <input type="text" class="search-input" name="search" placeholder="Search">
                                           <button class="search-icon" type="submit"><span></span></button>
                                       </div>
