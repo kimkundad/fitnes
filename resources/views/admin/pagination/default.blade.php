14a9a3927fdf6f78d9e9dfd9751cbcda3c3df2f4
kimkundad <kim.kundad@gmail.com>
2019-03-12 09:57:08 +0700

    golf

--- resources/views/admin/pagination/default.blade.php
+++ resources/views/admin/pagination/default.blade.php
@@ -1,17 +1,21 @@
+
+
+
 @if ($paginator->hasPages())
+<nav class="pagination-rounded" aria-label="Page navigation example">
     <ul class="pagination pagination">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="disabled"><span>«</span></li>
+            <li class="page-item disabled"><span>«</span></li>
         @else
             <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">«</a></li>
         @endif
 
         @if($paginator->currentPage() > 3)
-            <li class="hidden-xs"><a href="{{ $paginator->url(1) }}">1</a></li>
+            <li class="page-item hidden-xs"><a href="{{ $paginator->url(1) }}">1</a></li>
         @endif
         @if($paginator->currentPage() > 4)
-            <li><span>...</span></li>
+            <li class="page-item"><span>...</span></li>
         @endif
         @foreach(range(1, $paginator->lastPage()) as $i)
             @if($i >= $paginator->currentPage() - 2 && $i <= $paginator->currentPage() + 2)
@@ -23,17 +27,18 @@
             @endif
         @endforeach
         @if($paginator->currentPage() < $paginator->lastPage() - 3)
-            <li><span>...</span></li>
+            <li class="page-item"><span>...</span></li>
         @endif
         @if($paginator->currentPage() < $paginator->lastPage() - 2)
-            <li class="hidden-xs"><a href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
+            <li class="page-item hidden-xs"><a href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
         @endif
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">»</a></li>
+            <li class="page-item"><a href="{{ $paginator->nextPageUrl() }}" rel="next">»</a></li>
         @else
-            <li class="disabled"><span>»</span></li>
+            <li class="page-item disabled"><span>»</span></li>
         @endif
     </ul>
+    </nav>
 @endif
